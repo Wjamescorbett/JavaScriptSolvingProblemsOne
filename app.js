5b5b3e1afceb81b6067cba7525d9af4b7a894211
James Corbett <wjamescorbett@gmail.com>
2021-11-08 16:49:53 -0500

    Second function finished

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 function reverseString(stringParameter){
     reversed = ""
-    for(i = stringParameter.length-1; i >= 0; i--){
+    for(let i = stringParameter.length-1; i >= 0; i--){
         reversed += (stringParameter[i])
     }
     console.log(reversed)
 }
 reverseString("Hello")
+
+function title(stringParameter){
+    titleString = ""
+    titleString += (stringParameter[0].toUpperCase())
+    for(let i = 1; i < stringParameter.length; i++){
+        if(stringParameter[i] === " "){
+            titleString += " "
+            titleString += (stringParameter[i+1].toUpperCase())
+            i++
+        }
+        else{
+            titleString += stringParameter[i]
+        }
+    }
+    console.log(titleString)
+}
+
+title("hello world all this should work")
+
 
